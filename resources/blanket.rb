b26a8b4dd4348b428db2b3e58b83902aec8231af
Eric Hanko <eric.hanko1@gmail.com>
2017-11-27 02:33:27 -0800

    more debuggin

--- resources/blanket.rb
+++ resources/blanket.rb
@@ -34,11 +34,15 @@ load_current_value do |desired|
   puts '---> LOAD_CURRENT_VALUE [GO]'.colorize(:light_blue).bold
   blanket_helper('(4) Outside a method body inside load_current_value.')
 
+  property_printer('desired.is_unfolded', desired.is_unfolded, '[pre-setter] load_current_value')
+  property_printer('desired.height', desired.height, '[pre-setter] load_current_value')
+  property_printer('desired.width', desired.width, '[pre-setter] load_current_value')
+
   is_unfolded true
 
-  property_printer('desired.is_unfolded', desired.is_unfolded, 'load_current_value')
-  property_printer('desired.height', desired.height, 'load_current_value')
-  property_printer('desired.width', desired.width, 'load_current_value')
+  property_printer('desired.is_unfolded', desired.is_unfolded, '[post-setter] load_current_value')
+  property_printer('desired.height', desired.height, '[post-setter] load_current_value')
+  property_printer('desired.width', desired.width, '[post-setter] load_current_value')
 
   puts '---> LOAD_CURRENT_VALUE [STOP]'.colorize(:light_blue).bold
 end
@@ -46,8 +50,8 @@ end
 action :unfold do
   puts '---> ACTION [GO]'.colorize(:red).bold
 
-  action_class_helper('(1) Inside an action.')
-  blanket_helper('(5) Inside an action.')
+  action_class_helper('(5) Inside an action.')
+  blanket_helper('(6) Inside an action.')
 
   print "^^^^ #{current_value} ^^^^".colorize(:green).bold
   puts "\n"
@@ -58,9 +62,9 @@ action :unfold do
   converge_if_changed do
     puts '--> CONVERGE_IF_CHANGED [GO]'.colorize(:yellow).bold
 
-    action_class_helper('(2) Inside converge_if_changed.')
-    blanket_helper('(7) Inside converge_if_changed.')
-    property_printer('new_resource.is_unfolded', new_resource.is_unfolded, 'converge_if_changed')
+    action_class_helper('(7) Inside converge_if_changed.')
+    blanket_helper('(8) Inside converge_if_changed.')
+    property_printer('--> new_resource.is_unfolded', new_resource.is_unfolded, 'converge_if_changed')
     property_printer('new_resource.height', new_resource.height, 'converge_if_changed')
     property_printer('new_resource.width', new_resource.width, 'converge_if_changed')
     puts '---> CONVERGE_IF_CHANGED [STOP]'.colorize(:yellow).bold
@@ -69,8 +73,8 @@ action :unfold do
   puts "\n"
 
   execute 'echo "execute resource inside an action"' do
-    action_class_helper('(3) Inside a resource > inside an action.')
-    blanket_helper('(8) Inside a resource > inside an action.')
+    action_class_helper('(9) Inside a resource > inside an action.')
+    blanket_helper('(10) Inside a resource > inside an action.')
     live_stream true
   end
 
